df1ef522ce611c0c37f917f03883a9a8b82496ff
Todd Friedlich <tfriedlich@gmail.com>
2026-04-13 10:00:37 -0400

    v24j: Fix getAlbumName infinite self-recursion causing stack overflow on load

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ import config
 from pipeline import run_pipeline, confirm_and_upload, enhance_photo, pick_best_from_cluster
 from state import job_store
 
-APP_VERSION = "v24i"
+APP_VERSION = "v24j"
 
 app = FastAPI(title="Photo Curator")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
@@ -48,7 +48,7 @@ class AppLogger:
             "ts": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
             "level": level,
             "msg": msg,
-            "v": "v24i",
+            "v": "v24j",
         }
         if context:
             entry["ctx"] = context
